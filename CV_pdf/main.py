ce1b51eccf755c239115864d2243e0745c34cc7c
StAlZo <stalzol@mail.ru>
2024-06-30 23:41:51 +0300

    vfghvyrdyjgdfhh

--- CV_pdf/main.py
+++ CV_pdf/main.py
@@ -9,15 +9,18 @@ class Crave:
         self.__comvis = ComVis()
         self.__neunet = NeuralNetwork()
         self.__pdf_img = PDFTranslate(path)
+        self.spell = Speller(lang='ru')
 
     def create_txt(self):
         print("sdfsdf")
         self.__pdf_img.gen_png()
-        for i in self.__pdf_img.list_name:
-            print(i)
-            self.__comvis.set_image(f'ggg/' + i)
-            self.__neunet.set_text(self.__comvis.get_image())
-            print(self.__neunet.get_text())
+        with open("output.txt", "a") as f:
+            for i in self.__pdf_img.list_name:
+                print(i)
+                self.__comvis.set_image(f'ggg/' + i)
+                self.__neunet.set_text(self.__comvis.get_image())
+                print(self.__neunet.get_text())
+                f.write(self.spell(self.__neunet.get_text()))
 
 
 c = Crave("/home/stas/PycharmProjects/Test_task/ТЗ на выполнение работ.pdf")
